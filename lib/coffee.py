472e42d48f2c09445e00c2953da70001a0b5057b
kimmy-N <ayiakinashipai@gmail.com>
2026-02-21 21:18:52 +0300

    Complete Book and Coffee classes

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,24 @@
-#!/usr/bin/env python3
+# lib/coffee.py
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self._size = None  # internal variable for validation
+        self.size = size   # use setter for validation
+        self.price = price
+
+    # size property with validation
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if value in ["Small", "Medium", "Large"]:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+
+    # tip method
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
